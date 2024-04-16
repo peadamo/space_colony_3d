880b7b248498879518d76da0ec623853db8a6689
Your Namepablo <peadamo@gmail.com>
2024-04-16 16:35:35 -0300

    buildding from pod

--- space_colony_3d/assets/3d_models/ship/pod/pod.gd
+++ space_colony_3d/assets/3d_models/ship/pod/pod.gd
@@ -8,7 +8,7 @@ var is_on_use=false
 @onready var player_holder = $pod_y/pod_x/player_holder
 func start():
 	is_on_use=true
-
+var x_ray_on=false
 func _unhandled_input(event):
 	if is_on_use:
 		if event is InputEventMouseMotion:
@@ -16,7 +16,11 @@ func _unhandled_input(event):
 			pod_x.rotate_x(-event.relative.y*mouse_sensitivity)
 		
 		if Input.is_action_pressed("x_ray_vision"):
-			ship.active_xView_vision()
+			x_ray_on=!x_ray_on
+			if x_ray_on:
+				ship.active_xView_vision()
+			else:
+				ship.inactive_xView_vision()
 		
 @onready var giroscope_start = $pod_y/pod_x/giroscope_start
 @onready var giroscope_front = $pod_y/pod_x/giroscope_start/giroscope_front
@@ -63,5 +67,71 @@ func _physics_process(delta):
 			
 			
 		move_and_slide()
+		calc_pointer_ref_position()
 		
+		
+func update_pointer_pos():
+	var spaceState=get_world_3d().direct_space_state
+	var mousePos = Vector2(1152.0/2,648.0/2)
+	var camera = get_tree().root.get_camera_3d()
+	var rayOrigin = camera.project_ray_origin(mousePos)
+	var rayEnd = rayOrigin + camera.project_ray_normal(mousePos) *2000
+	var Parameters = PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd,0x2)
+	var rayArray = spaceState.intersect_ray(Parameters)
+	if rayArray.has("position"):
+		return rayArray["position"]
+	return Vector3.ZERO
 	
+func calc_pointer_ref_position():
+	var new_position=update_pointer_pos()
+	
+	var value_x
+	var value_x_sign=1
+	if new_position.x<0:
+		value_x_sign=-1
+	var value_x_module=new_position.x*value_x_sign
+	var value_x_decimal=value_x_module-floor(value_x_module)
+	if value_x_decimal>0.5:
+		value_x=(floor(value_x_module)+1)*value_x_sign
+	else:
+		value_x=(floor(value_x_module))*value_x_sign
+	
+	var value_z
+	var value_z_sign=1
+	if new_position.z<0:
+		value_z_sign=-1
+	var value_z_module=new_position.z*value_z_sign
+	var value_z_decimal=value_z_module-floor(value_z_module)
+	if value_z_decimal>0.5:
+		value_z=(floor(value_z_module)+1)*value_z_sign
+	else:
+		value_z=(floor(value_z_module))*value_z_sign
+		
+	var value_y
+	var value_y_sign=1
+	if new_position.y<0:
+		value_y_sign=-1
+	var value_y_module=new_position.y*value_y_sign
+	var value_y_decimal=value_y_module-floor(value_y_module)
+	if value_y_decimal>0.5:
+		value_y=(floor(value_y_module)+1)*value_y_sign
+	else:
+		value_y=(floor(value_y_module))*value_y_sign
+		
+	if value_y>0:
+		value_y-=0.5
+	
+	var cal_corrected_position = Vector3(value_x,value_y,value_z)
+	
+	
+	
+	if cal_corrected_position != corrected_position:
+		corrected_position=cal_corrected_position
+		update_mesh_ref_position()
+			
+var corrected_position
+var is_floor_blue_print=true
+@onready var pointer_mesh_ref = $pointer_mesh_ref
+
+func update_mesh_ref_position():
+	pointer_mesh_ref.global_position=Vector3(corrected_position.x,0,corrected_position.z)
--- space_colony_3d/ship/base_ship.gd
+++ space_colony_3d/ship/base_ship.gd
@@ -1,13 +1,59 @@
 extends Node3D
 @onready var ship_buildings = $ship_buildings
 @onready var floor = $floor
+@onready var hull_walls = $hull_walls
 
 func ship_get_floor_nodes():
 	return $floor.get_children()
 
 var floor_meshes:Array=[]
 
+var x_ray_meshes : Array = []
+
+const PROP_BLUEPRINT_SKYBLUE = preload("res://shaders_and_materials/prop_blueprint_skyblue.tres")
+@onready var roof = $roof
+
 func active_xView_vision():
-	var floor_nodes=ship_get_floor_nodes()
-	print("x_vision")
+	x_ray_meshes.clear()
 	
+	var wall_nodes=hull_walls.get_children()
+	for node in wall_nodes:
+		var node_children = node.get_children()
+		var node_mesh_container = null
+		
+		for children in node_children:
+			if children.is_in_group("mesh_container"):
+				node_mesh_container=children
+		
+		if node_mesh_container != null :
+			var mesh_container_children = node_mesh_container.get_children()
+			for mesh in mesh_container_children:
+				x_ray_meshes.append(mesh)
+				
+	var roof_nodes=roof.get_children()
+	for node in roof_nodes:
+		var node_children = node.get_children()
+		var node_mesh_container = null
+		
+		for children in node_children:
+			if children.is_in_group("mesh_container"):
+				node_mesh_container=children
+		
+		if node_mesh_container != null :
+			var mesh_container_children = node_mesh_container.get_children()
+			for mesh in mesh_container_children:
+				x_ray_meshes.append(mesh)
+				
+				
+				
+				
+				
+				
+				
+				
+	for mesh in x_ray_meshes:
+		mesh.material_override=PROP_BLUEPRINT_SKYBLUE
+	
+func inactive_xView_vision():
+	for mesh in x_ray_meshes:
+		mesh.material_override=null
